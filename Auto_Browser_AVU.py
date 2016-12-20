48ba42a312bd6f1f663bf46622fd514c92ac2fbb
Vu NGUYEN <vunguyen.xbt@gmail.com>
2016-12-20 13:04:10 +0700

    add timing

--- Auto_Browser_AVU.py
+++ Auto_Browser_AVU.py
@@ -74,13 +74,15 @@ def connect_vpn(refresh, number_machine):
     elif get_params('OpenVPN') == 1:
         ip = tuple(open('ressources\config_ip.txt', 'r'))
         cmd = 'C:\Program Files\OpenVPN\\bin\openvpn.exe'
-        params = '--remote ' + ip[number_machine] + ' --status C:\status.log --log C:\logChangeIP.txt --tls-client --client ' \
-                                    '--dev tun --proto udp --port 1197 --lport 53 --persist-key --persist-tun ' \
-                                    '--ca data\ca.crt --comp-lzo --mute 3 --tun-mtu 1400 --mssfix 1360 ' \
-                                    '--auth-user-pass data\\auth.txt --reneg-sec 0 --keepalive 10 120 ' \
-                                    '--route-method exe --route-delay 2 --verb 3 --auth-nocache ' \
-                                    '--crl-verify data\crl.pem --remote-cert-tls server --block-outside-dns ' \
-                                    '--cipher aes-256-cbc --auth sha256'
+        params = '--remote ' + ip[number_machine] + ' --status C:\status.log --log C:\logChangeIP.txt ' \
+                                                    '--tls-client --client --dev tun --proto udp ' \
+                                                    '--port 1197 --lport 53 --persist-key ' \
+                                                    '--persist-tun --ca data\ca.crt --comp-lzo --mute 3 ' \
+                                                    '--tun-mtu 1400 --mssfix 1360 --auth-user-pass data\\auth.txt ' \
+                                                    '--reneg-sec 0 --keepalive 10 120 --route-method exe ' \
+                                                    '--route-delay 2 --verb 3 --auth-nocache ' \
+                                                    '--crl-verify data\crl.pem --remote-cert-tls server ' \
+                                                    '--block-outside-dns --cipher aes-256-cbc --auth sha256'
         cmd = cmd + params
         check_output(cmd, shell=True)
 
