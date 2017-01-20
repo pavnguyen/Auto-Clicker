13221ec0a0231e87548e450f3c27276884321d6b
Vu NGUYEN <vunguyen.xbt@gmail.com>
2017-01-20 17:34:31 +0700

    optimize

--- change_md5.py
+++ change_md5.py
@@ -6,25 +6,37 @@ import sys
 
 try:
     if len(sys.argv) > 1:
-        path = sys.argv[1]
+        NBR_CHANNEL = int(sys.argv[1])
     else:
-        path = 'videos/'
+        print('Enter number of channel to create: ')
+        NBR_CHANNEL = int(raw_input())
+
+    path = 'videos/'
     dirs = os.listdir(path)
 except:
     print('Error Folder!!!')
     pass
 
 try:
-    for file_source in dirs:
-        print('[File to change MD5]  ' + file_source)
-        old_md5 = hashlib.md5(open(path + file_source, 'rb').read()).hexdigest()
-        file_opened = open(path + file_source, 'rb').read()
-        with open(path + file_source, 'wb') as new_file:
+    for file in dirs:
+        for i in range(0, NBR_CHANNEL):
             value_random = random.randint(1, 99) * random.randint(1, 99)
-            new_file.write(file_opened + '\0' * value_random)  # add a null to change the file content
+            if i == 0:
+                file_source = path + file
+            else:
+                file_source = path + str(i) + '_' + file
 
-        new_md5 = hashlib.md5(open(path + file_source, 'rb').read()).hexdigest()
-        print(' ' * 22 + '[Old MD5] ' + old_md5 + ' ---> [New MD5] ' + new_md5)
+            print('[File to change MD5]  ' + file_source)
+
+            old_md5 = hashlib.md5(open(file_source, 'rb').read()).hexdigest()
+            file_opened = open(file_source, 'rb').read()
+
+            i += 1
+            with open(path + str(i) + '_' + file, 'wb') as new_file:
+                new_file.write(file_opened + '\0' * value_random)  # add a null to change the file content
+
+            new_md5 = hashlib.md5(open(path + str(i) + '_' + file, 'rb').read()).hexdigest()
+            print(' ' * 22 + '[Old MD5] ' + old_md5 + ' ---> [New MD5] ' + new_md5)
     print('DONE! DONE! DONE!')
 except:
     print('Error: change MD5 failed!')
