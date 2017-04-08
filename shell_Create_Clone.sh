2fe04062d98ef6bb18dbcd8fa073c74803bde08d
linux <vunguyen.xbt@gmail.com>
2017-04-08 15:54:21 +1000

    add shell create

---
+++ shell_Create_Clone.sh
@@ -0,0 +1,107 @@
+vmrun deleteVM /home/linux/VirtualMachine/LN1/1.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN2/2.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN3/3.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN4/4.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN5/5.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN6/6.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN7/7.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN8/8.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN9/9.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN10/10.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN11/11.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN12/12.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN13/13.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN14/14.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN15/15.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN16/16.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN17/17.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN18/18.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN19/19.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN20/20.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN21/21.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN22/22.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN23/23.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN24/24.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN25/25.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN26/26.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN27/27.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN28/28.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN29/29.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN30/30.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN31/31.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN32/32.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN33/33.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN34/34.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN35/35.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN36/36.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN37/37.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN38/38.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN39/39.vmx
+vmrun deleteVM /home/linux/VirtualMachine/LN40/40.vmx
+
+
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN1/1.vmx linked -cloneName=LN1
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN2/2.vmx linked -cloneName=LN2
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN3/3.vmx linked -cloneName=LN3
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN4/4.vmx linked -cloneName=LN4
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN5/5.vmx linked -cloneName=LN5
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN6/6.vmx linked -cloneName=LN6
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN7/7.vmx linked -cloneName=LN7
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN8/8.vmx linked -cloneName=LN8
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN9/9.vmx linked -cloneName=LN9
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN10/10.vmx linked -cloneName=LN10
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN11/11.vmx linked -cloneName=LN11
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN12/12.vmx linked -cloneName=LN12
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN13/13.vmx linked -cloneName=LN13
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN14/14.vmx linked -cloneName=LN14
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN15/15.vmx linked -cloneName=LN15
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN16/16.vmx linked -cloneName=LN16
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN17/17.vmx linked -cloneName=LN17
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN18/18.vmx linked -cloneName=LN18
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN19/19.vmx linked -cloneName=LN19
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN20/20.vmx linked -cloneName=LN20
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN21/21.vmx linked -cloneName=LN21
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN22/22.vmx linked -cloneName=LN22
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN23/23.vmx linked -cloneName=LN23
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN24/24.vmx linked -cloneName=LN24
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN25/25.vmx linked -cloneName=LN25
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN26/26.vmx linked -cloneName=LN26
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN27/27.vmx linked -cloneName=LN27
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN28/28.vmx linked -cloneName=LN28
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN29/29.vmx linked -cloneName=LN29
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN30/30.vmx linked -cloneName=LN30
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN31/31.vmx linked -cloneName=LN31
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN32/32.vmx linked -cloneName=LN32
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN33/33.vmx linked -cloneName=LN33
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN34/34.vmx linked -cloneName=LN34
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN35/35.vmx linked -cloneName=LN35
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN36/36.vmx linked -cloneName=LN36
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN37/37.vmx linked -cloneName=LN37
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN38/38.vmx linked -cloneName=LN38
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN39/39.vmx linked -cloneName=LN39
+vmrun clone /home/linux/VirtualMachine/Ubuntu/Ubuntu.vmx /home/linux/VirtualMachine/LN40/40.vmx linked -cloneName=LN40
+
+vmrun start /home/linux/VirtualMachine/LN1/1.vmx
+vmrun start /home/linux/VirtualMachine/LN2/2.vmx
+vmrun start /home/linux/VirtualMachine/LN3/3.vmx
+vmrun start /home/linux/VirtualMachine/LN4/4.vmx
+vmrun start /home/linux/VirtualMachine/LN5/5.vmx
+vmrun start /home/linux/VirtualMachine/LN6/6.vmx
+vmrun start /home/linux/VirtualMachine/LN7/7.vmx
+vmrun start /home/linux/VirtualMachine/LN8/8.vmx
+vmrun start /home/linux/VirtualMachine/LN9/9.vmx
+vmrun start /home/linux/VirtualMachine/LN10/10.vmx
+vmrun start /home/linux/VirtualMachine/LN11/11.vmx
+vmrun start /home/linux/VirtualMachine/LN12/12.vmx
+vmrun start /home/linux/VirtualMachine/LN13/13.vmx
+vmrun start /home/linux/VirtualMachine/LN14/14.vmx
+vmrun start /home/linux/VirtualMachine/LN15/15.vmx
+vmrun start /home/linux/VirtualMachine/LN16/16.vmx
+vmrun start /home/linux/VirtualMachine/LN17/17.vmx
+vmrun start /home/linux/VirtualMachine/LN18/18.vmx
+vmrun start /home/linux/VirtualMachine/LN19/19.vmx
+vmrun start /home/linux/VirtualMachine/LN20/20.vmx
+vmrun start /home/linux/VirtualMachine/LN21/21.vmx
+vmrun start /home/linux/VirtualMachine/LN22/22.vmx
+vmrun start /home/linux/VirtualMachine/LN23/23.vmx
+vmrun start /home/linux/VirtualMachine/LN24/24.vmx
